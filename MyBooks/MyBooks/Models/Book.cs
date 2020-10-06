c60e2d4d5deb22b6e2e3899bd2ad5f6cc8381166
pasternak2048 <pasternak2048@gmail.com>
2020-10-07 02:06:59 +0300

    Add model Book

---
+++ MyBooks/MyBooks/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBooks.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorsName { get; set; }
+        public string AuthorsSurname { get; set; }
+        public int YearOfPublish { get; set; }
+        public int GenreId { get; set; }
+    }
+}
