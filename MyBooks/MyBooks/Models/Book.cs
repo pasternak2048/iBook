3b336c84b8fc8689642b8998b19b955ce668d97a
pasternak2048 <pasternak2048@gmail.com>
2020-10-07 02:24:43 +0300

    Add model Genre and create relationship

--- MyBooks/MyBooks/Models/Book.cs
+++ MyBooks/MyBooks/Models/Book.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyBooks.Models
 {
+    [Serializable]
     public class Book
     {
-        public int Id { get; set; }
+        [Key]
+        public int BookId { get; set; }
         public string Name { get; set; }
         public string AuthorName { get; set; }
         public string AuthorSurname { get; set; }
         public int YearOfPublish { get; set; }
+        [ForeignKey("Genre")]
         public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+
     }
 }
---
+++ MyBooks/MyBooks/Models/Genre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Serialization;
+
+
+namespace MyBooks.Models
+{
+    [Serializable]
+    [XmlInclude(typeof(Book))]
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+
+        [XmlIgnore]
+        public virtual List<Book> Books { get; set; }
+        public Genre()
+        {
+            Books = new List<Book>();
+        }
+    }
+}
