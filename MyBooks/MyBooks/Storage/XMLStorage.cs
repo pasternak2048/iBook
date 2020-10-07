67138349b21717451da7031473a4ee29c268741c
pasternak2048 <pasternak2048@gmail.com>
2020-10-07 20:52:51 +0300

    Modify method ReturnAllBooks to generic ReturnAllItemsFromXml

--- MyBooks/MyBooks/Storage/XMLStorage.cs
+++ MyBooks/MyBooks/Storage/XMLStorage.cs
@@ -10,18 +10,16 @@ namespace MyBooks.Storage
 {
     public class XMLStorage
     {
-        string _xmlPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//MyBooks.xml";
-
-        public List<Book> ReturnAllBooksFromXml()
+      
+        public List<T> ReturnAllItemsFromXml<T>(string _xmlPath) where T: class
         {
-            Type[] _types = new Type[] { typeof(Book) };
-            List<Book> _books = new List<Book>();
-            XmlSerializer reader = new XmlSerializer(_books.GetType(),_types);
+            Type[] _types = new Type[] { typeof(T) };
+            List<T> _items = new List<T>();
+            XmlSerializer reader = new XmlSerializer(_items.GetType(), _types);
             System.IO.StreamReader file = new System.IO.StreamReader(_xmlPath);
-            _books = (List<Book>)reader.Deserialize(file);
+            _items = (List<T>)reader.Deserialize(file);
             file.Close();
-            return _books;
+            return _items;   
         }
-
     }
 }
