d6c602ad8e38872bdd67e6369841f253f1a15b21
pasternak2048 <pasternak2048@gmail.com>
2020-10-08 11:13:35 +0300

    Add many changes

--- MyBooks/MyBooks/Controllers/HomeController.cs
+++ MyBooks/MyBooks/Controllers/HomeController.cs
@@ -35,9 +35,48 @@ namespace MyBooks.Controllers
             return View();
         }
 
-        public void StartUp ()
+        [HttpGet]
+        public ActionResult AddBook()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public string AddBook(Book _book)
         {
             
+            if (_book.BookId==0)
+            {
+                return "Wrong value: BookID. (cannot have a value of 0 and must be a numeric)";
+            }
+            if (IsBookExist(_book.BookId))
+            {
+                ViewBag.Message = "Wrong value: BookID. (already exist).";
+                return "Wrong value: BookID. (already exist).";
+            }
+ 
+            if (_book.AuthorName==null || !IsTextHasWrongSymbols(_book.AuthorName))
+            {
+                return "Wrong value: Author Name. (has wrong symbols).";
+            }
+            if (_book.AuthorSurname==null || !IsTextHasWrongSymbols(_book.AuthorSurname))
+            {
+                return "Wrong value: Author Surname. (has wrong symbols).";
+            }
+            if (_book.YearOfPublish < 1500 || _book.YearOfPublish > DateTime.Now.Year)
+            {
+                return $"Wrng year. Year must be > 1500 or < {DateTime.Now.Year}";
+            }
+            if (!IsGenreExist(_book.GenreId))
+            {
+                return "Wrong value: Genre does not exist.";
+            }
+            _book.Genre = new XMLStorage().ReturnAllItemsFromXml<Genre>(_xmlPathGenres).Find(i => i.GenreId == _book.GenreId);
+            new XMLStorage().AddBook(_book, _xmlPathBooks);
+            return "Saved";
+        }
+        public void StartUp ()
+        {
             if (System.IO.File.Exists(_xmlPathGenres) == false)
             {
                 XDocument xdoc = new XDocument(new XElement("ArrayOfGenre"));
@@ -49,6 +88,61 @@ namespace MyBooks.Controllers
                 XDocument xdoc = new XDocument(new XElement("ArrayOfBook"));
                 xdoc.Save(_xmlPathBooks);
             }
+        }
+
+
+        public bool IsTextHasWrongSymbols(string _userInput)
+        {
+            bool _checker = true;
+            foreach (char c in _userInput)
+            {
+                if (Char.IsNumber(c) || Char.IsPunctuation(c) || Char.IsWhiteSpace(c) || Char.IsSeparator(c) || Char.IsSurrogate(c) || Char.IsSymbol(c))
+                {
+                    _checker = false;
+                }
+            }
+            return _checker;
+        }
+        public bool IsTextNumeric(string _userInput)
+        {
+            bool _checker = true;
+            foreach (char c in _userInput)
+            {
+                if (!Char.IsNumber(c))
+                {
+                    _checker = false;
+                }
+                
+            }
+            return _checker;
+        }
+
+        bool IsGenreExist(int _userChoise)
+        {
+            List<Genre> _genres = new XMLStorage().ReturnAllItemsFromXml<Genre>(_xmlPathGenres);
+            bool _checkIsValueExist = false;
+            foreach (Genre i in _genres)
+            {
+                if (i.GenreId == _userChoise)
+                {
+                    _checkIsValueExist = true;
+                }
+            }
+            return _checkIsValueExist;
+        }
+
+        bool IsBookExist(int _userChoise)
+        {
+            List<Book> _books = new XMLStorage().ReturnAllItemsFromXml<Book>(_xmlPathBooks);
+            bool _checkIsValueExist = false;
+            foreach (Book i in _books)
+            {
+                if (i.BookId == _userChoise)
+                {
+                    _checkIsValueExist = true;
+                }
+            }
+            return _checkIsValueExist;
         }
     }
 }
