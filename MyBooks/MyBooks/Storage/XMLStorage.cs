c96ab423906005e4e73714ec0ea6aacc04db3717
pasternak2048 <pasternak2048@gmail.com>
2020-10-08 08:30:09 +0300

    Add method AddBook

--- MyBooks/MyBooks/Storage/XMLStorage.cs
+++ MyBooks/MyBooks/Storage/XMLStorage.cs
@@ -23,6 +23,23 @@ namespace MyBooks.Storage
             xDoc.Save(_xmlPathGenres);
         }
 
+        public void AddBook(Book _book, string _xmlPathBooks)
+        {
+            XDocument xDoc = XDocument.Load(_xmlPathBooks);
+            xDoc.Element("ArrayOfBook").Add(
+                new XElement("Book",
+                new XElement("BookId", _book.BookId),
+                new XElement("Name", _book.Name),
+                new XElement("AuthorName", _book.AuthorName),
+                new XElement("AuthorSurname", _book.AuthorSurname),
+                new XElement("YearOfPublish", _book.YearOfPublish),
+                new XElement("GenreId", _book.Genre.GenreId),
+                new XElement("Genre",
+                    new XElement("GenreId", _book.Genre.GenreId),
+                    new XElement("Name", _book.Genre.Name))));
+            xDoc.Save(_xmlPathBooks);
+        }
+
         public List<T> ReturnAllItemsFromXml<T>(string _xmlPath) where T: class
         {
             Type[] _types = new Type[] { typeof(T) };
