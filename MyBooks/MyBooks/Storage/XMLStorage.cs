c33d7037a270417d245386d7322e4a54faf90f70
pasternak2048 <pasternak2048@gmail.com>
2020-10-08 08:26:27 +0300

    Add method AddGenre

--- MyBooks/MyBooks/Storage/XMLStorage.cs
+++ MyBooks/MyBooks/Storage/XMLStorage.cs
@@ -5,12 +5,24 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Xml.Serialization;
+using System.Xml.Linq;
 
 namespace MyBooks.Storage
 {
     public class XMLStorage
     {
-      
+
+
+        public void AddGenre(Genre _genre, string _xmlPathGenres)
+        {
+            XDocument xDoc = XDocument.Load(_xmlPathGenres);
+            xDoc.Element("ArrayOfGenre").Add(
+                        new XElement("Genre",
+                        new XElement("GenreId", _genre.GenreId),
+                        new XElement("Name", _genre.Name)));
+            xDoc.Save(_xmlPathGenres);
+        }
+
         public List<T> ReturnAllItemsFromXml<T>(string _xmlPath) where T: class
         {
             Type[] _types = new Type[] { typeof(T) };
